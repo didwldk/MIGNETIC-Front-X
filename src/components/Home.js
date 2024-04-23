adaf781c91716448448de6188e7e101c8628bb9e
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-04-23 22:43:27 +0900

    home화면 구현

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
-import '../css/reset.css'
 import '../css/Home.css'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+    const navigate = useNavigate()
+
+    const handleOnclickTest = () => {
+        navigate('/test')
+    }
+
+    const handleOnclickView= ()=> {
+        navigate('/view')
+    }
+
     return (
         <div className='home'>
-            
+            <div className='btnContainer'>
+                <button onClick={handleOnclickTest}>검사하기</button>
+                <button onClick={handleOnclickView}>핫플보기</button>
+            </div>
+
+            <i class="bi bi-info-circle-fill"></i>
         </div>
     )
 }
