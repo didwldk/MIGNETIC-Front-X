6c9e8d42e45c6466b01019699f031ecadd1b50f8
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2024-04-25 11:21:19 +0900

    모달창구현

--- src/components/Hot_place.js
+++ src/components/Hot_place.js
@@ -1,34 +1,59 @@
-import '../css/reset.css';
-import '../css/Hot_place';
+// import '../css/reset.css';
+// import '../css/Hot_place';
 
-import { useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
-//스크립트로 kakao maps api를 심어서 가져오면 window 전역에 객체에 들어가게 됨
-//그런데 함수형 컴포넌트에서는 이를 바로 인식 못함
-//그래서 상단에 
-const {kakao} = window;
-//를 작성하여 함수형 컴포넌트에 인지시키고 window에서 kakao 객체를 뽑아서 사용
+function Hot_place(){
+    const {kakao} = window;
 
-function Kakao() {
+    const container = useRef(null)
+    const options = {
+        center: new kakao.maps.LatLng(37.4661018035835, 126.931927855676),
+        level: 3
+    }
+
     useEffect(() => {
-        const container = document.getElementById('map');
-        //지도를 담을 영역의 DOM 레퍼런스
-        const options = {
-            center: new kakao.maps.LatLng(37.4661018035835, 126.931927855676),
-            //지도의 중심 좌표 (미림마이스터고)
-            level: 3
-        };
-        const map = new kakao.maps.Map(container, options);
-    }, [])
-    // useEffect를 이용해 지도를 가지고 있는 컴포넌트가 처음 렌더링 될 때 지도를 띄우기 위해
-    // 두번째 인자를 빈배열로 설정
+        new kakao.maps.Map(container, options)
+        return () => {};
+    }, []);
 
-    return (
-        <div class = "place">
-            <div id = "map"></div>
+    return(
+        <div id = "map" ref={container} style={{
+            width: '1366px',
+            height: '700px'
+        }}>
         </div>
-        
     )
 }
 
-export default Kakao;
+// const Hot_place = () => {
+//     const [map,setMap] = useState(null);
+
+//     useEffect(() => {
+//         const container = document.getElementById('map');
+//         //지도를 담을 영역의 DOM 레퍼런스
+//         const options = {
+//             center: new kakao.maps.LatLng(37.4661018035835, 126.931927855676),
+//             //지도의 중심 좌표 (미림마이스터고)
+//             level: 3
+//         };
+//         const map = new kakao.maps.Map(container, options);
+//     }, [])
+//     // useEffect를 이용해 지도를 가지고 있는 컴포넌트가 처음 렌더링 될 때 지도를 띄우기 위해
+//     // 두번째 인자를 빈배열로 설정
+
+//     return (
+//         <div class = "place" style={{
+//             width: '1860px',
+//             height: '776px'
+//         }}>
+//             <div id = "map" style={{
+//                 width: '1366px',
+//                 height: '700px'
+//             }}></div>
+//         </div>
+        
+//     )
+// }
+
+export default Hot_place;
