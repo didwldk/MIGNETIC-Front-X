8c1cec9cf66c7b0a1cd56f18fee0ef406c683c1e
hxxjx523 <hhj05230@gmail.com>
2024-04-26 08:47:13 +0900

    게사판 수정

--- src/components/NoticeBoard.js
+++ src/components/NoticeBoard.js
@@ -1,9 +1,20 @@
-import bgImg2 from '../images/NoticeBoardBG.png'
 import '../css/NoticeBoard.css'
+import bgImg2 from '../images/NoticeBoardBG.png'
 import envelop1 from '../images/Envelope1.png'
 import envelop2 from '../images/Envelope2.png'
+import { useState } from 'react'
 
 function NoticeBoard(){
+
+    const [showLetter, setShowLetter] = useState(false);
+
+    const handleMouseOver = () => {
+        setShowLetter(true);
+    };
+
+    const handleMouseOut = () => {
+        setShowLetter(false);
+    };
 
     document.body.style.backgroundImage = `url(${bgImg2})`;
     // document.body.style.backgroundColor = "#3373FF";
@@ -12,12 +23,10 @@ function NoticeBoard(){
         <div className="NLetter">
             <div className="Nenvelop1">
                 <img src={envelop1}/>
+                <div className="NletterPaper" style={{ visibility: showLetter ? 'visible' : 'hidden' }}>
             </div>
-            <div className="NletterPaper">
-                {/* <input type="text" className="InputText" translate="여기에 편지"/> */}
-                <textarea className="NInputText" placeholder="내용을 입력해 주세요."/>
             </div>
-            <div className="Nenvelop2">
+            <div className="Nenvelop2" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
                 <img src={envelop2}/>
             </div>
         </div>
