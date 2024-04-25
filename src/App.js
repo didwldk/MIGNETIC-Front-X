8919dc5db2541d65f0b4b65a5a9b9325e04a6042
hxxjx523 <hhj05230@gmail.com>
2024-04-25 13:31:33 +0900

    루트 추가

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Main from './pages/Main'
 import Who from './pages/Who'
-import Letterpage from './pages/Letterpage';
-import Hot_place_Page from './pages/Hot_place_Page';
+import LetterPage from './pages/LetterPage'
+import Hot_place_Page from './pages/Hot_place_Page'
+import NoticeBoard from './components/NoticeBoard'
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />}></Route>
           <Route path='/who' element={<Who />}></Route>
-          <Route path='/letter' element={<Letterpage />}></Route>
+          <Route path='/letter' element={<LetterPage />}></Route>
           <Route path='/hotplace' element={<Hot_place_Page />}></Route>
+          <Route path='/NoticeBoard' element={<NoticeBoard />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
