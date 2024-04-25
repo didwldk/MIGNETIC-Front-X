b884987411e5c67e2bd2ef58f21501075709bfc8
hxxjx523 <hhj05230@gmail.com>
2024-04-25 13:31:18 +0900

    게시판 추가

---
+++ src/pages/NoticeBoardPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NoticeBoard from '../components/NoticeBoard'
+
+function NoticeBoardPage() {
+    return (
+        <div>
+            <NoticeBoard />
+        </div>
+    )
+}
+
+export default NoticeBoardPage
