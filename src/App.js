3d5667b38f85e0b3349c189474bf57ce4689cfd1
hxxjx523 <hhj05230@gmail.com>
2024-04-25 13:57:51 +0900

    루트 수정

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Main from './pages/Main'
 import Who from './pages/Who'
-<<<<<<< HEAD
-import LetterPage from './pages/LetterPage'
-import Hot_place_Page from './pages/Hot_place_Page'
-import NoticeBoard from './components/NoticeBoard'
-=======
-import Letterpage from './pages/Letterpage';
+import LetterPage from './pages/LetterPage';
 import HotPlacePage from './pages/HotPlacePage';
->>>>>>> d6576c978e3c82692647514e31499d7c5be58c77
+import NoticeBoard from './components/NoticeBoard'
 
 function App() {
   return (
@@ -18,14 +13,9 @@ function App() {
         <Routes>
           <Route path='/' element={<Main />}></Route>
           <Route path='/who' element={<Who />}></Route>
-<<<<<<< HEAD
           <Route path='/letter' element={<LetterPage />}></Route>
-          <Route path='/hotplace' element={<Hot_place_Page />}></Route>
-          <Route path='/NoticeBoard' element={<NoticeBoard />}></Route>
-=======
-          <Route path='/letter' element={<Letterpage />}></Route>
           <Route path='/hotplace' element={<HotPlacePage />}></Route>
->>>>>>> d6576c978e3c82692647514e31499d7c5be58c77
+          <Route path='/NoticeBoard' element={<NoticeBoard />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
