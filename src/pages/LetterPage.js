66f7c2ace08a6d092f313a895d4e5eab2fef6676
hxxjx523 <hhj05230@gmail.com>
2024-04-26 09:05:42 +0900

    편지 페이지 수정

---
+++ src/pages/LetterPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Letter from '../components/Letter'
+
+function LetterPage() {
+    return (
+        <div>
+            <Letter />
+        </div>
+    )
+} 
+
+export default LetterPage
